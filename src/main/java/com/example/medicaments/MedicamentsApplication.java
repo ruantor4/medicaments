4add12459843af0f8ca52aa9b47da1e1eb7d1f9b
Ruan Torquato <ruan.tor4@hotmail.com>
2024-07-02 13:19:55 -0300

    PROJECT START

---
+++ src/main/java/com/example/medicaments/MedicamentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicaments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicamentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicamentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medicaments/MedicamentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicaments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicamentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
